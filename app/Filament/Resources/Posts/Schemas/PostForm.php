736e648f453913e9bcf037e4483e243edbde7d8c
wesley <340308@student.mboutrecht.nl>
2025-09-30 13:08:03 +0200

    video 5

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -9,6 +9,8 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class PostForm
@@ -17,21 +19,31 @@ class PostForm
     {
         return $schema
             ->components([
-                TextInput::make('title')
-                    ->required(),
-                TextInput::make('slug')
-                    ->required(),
-                ColorPicker::make('color')
-                    ->required(),
-                FileUpload::make('thumbnail')->disk('public')->directory('thumbnails'),
-                Select::make('category_id')
-                    ->relationship('category', 'name')
-                    ->required(),
-                MarkdownEditor::make('content')
-                    ->columnSpanFull(),
-                TagsInput::make('tags'),
-                Toggle::make('published')
-                    ->required(),
-            ]);
+                Section::make('Create a post')->schema([
+                    TextInput::make('title')
+                        ->required(),
+                    TextInput::make('slug')
+                        ->required(),
+                    Select::make('category_id')
+                        ->relationship('category', 'name')
+                        ->required(),
+                    ColorPicker::make('color')
+                        ->required(),
+                    MarkdownEditor::make('content')
+                        ->columnSpanFull(),
+
+                    Toggle::make('published')
+                        ->required(),
+                ])->columnSpan(2),
+
+                Group::make()->schema([
+                    Section::make('image')->schema([
+                        FileUpload::make('thumbnail')->disk('public')->directory('thumbnails'),
+                    ]),
+                    Section::make('meta')->schema([
+                        TagsInput::make('tags'),
+                    ]),
+                ]),
+            ])->columns(3);
     }
 }
